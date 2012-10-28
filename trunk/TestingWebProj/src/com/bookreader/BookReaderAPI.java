6d82859c4be0e2ee12b10ec4d12740fc014c3d85
Renis Makadia <renismakadia@yahoo.com>
2012-10-28 13:23:58 -0700

    1028 changes DB related

--- trunk/TestingWebProj/src/com/bookreader/BookReaderAPI.java
+++ trunk/TestingWebProj/src/com/bookreader/BookReaderAPI.java
@@ -1,6 +1,9 @@
 package com.bookreader;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -11,6 +14,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
+import com.data.DBController;
 
 @Path("/br")
 public class BookReaderAPI {
@@ -34,7 +39,15 @@ public class BookReaderAPI {
 			) throws IOException
 	{
 		System.out.println("POST call: phoneNum" + phoneNum + "logType" + logType + "logValue" + logValue );
-		return "POST call: Hello BookReader - i will handel log entries";
+		DBController dbController = new DBController();
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		
+		
+		String temp = dbController.addLogEntry("bookreader", logType, logValue, phoneNum , dateFormat.format(date));
+		 //addLogEntry(String appName, String logType, String logValue, String time, String phoneNum)
+		return "POST call: Hello BookReader - i will handel log entries ="+ temp;
 		
 	}
 
